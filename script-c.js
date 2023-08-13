554db9985cad3aa5ec262098e2818b2d8bcd040d
BenoAbriyan <128135053+benoas988@users.noreply.github.com>
2023-08-13 20:15:30 +0700

    Add files via upload

---
+++ script-c.js
@@ -0,0 +1,34 @@
+document.getElementById("home").addEventListener("click", function(event){
+      event.preventDefault();
+      window.location.href = "home.html";
+  });
+  document.getElementById("port").addEventListener("click", function(event){
+      event.preventDefault();
+      window.location.href = "portfolio..html";
+  });
+  document.getElementById("tentang").addEventListener("click", function(event){
+      event.preventDefault();
+      window.location.href = "home.html";
+  });
+  document.getElementById("ex").addEventListener("click", function(event){
+      event.preventDefault();
+      window.location.href = "experience.html";
+  });
+  document.getElementById("imel").addEventListener("click", function(event){
+      event.preventDefault();
+      window.location.href = "https://beno.as988@gmail.com";
+  });
+  document.getElementById("ig").addEventListener("click", function(event){
+      event.preventDefault();
+      window.location.href = "https://instagram.com/benoabryn?igshid=MjEwN2IyYWYwYw==";
+  });
+  document.getElementById("ngl").addEventListener("click", function(event){
+      event.preventDefault();
+      window.location.href = "https://ngl.link/benoabryn";
+  });
+  document.getElementById("web").addEventListener("click", function(event){
+      event.preventDefault();
+      window.location.href = "https://www.google.com";
+  });
+
+  
---
+++ script-ex.js
@@ -0,0 +1,16 @@
+document.getElementById("home").addEventListener("click", function(event){
+      event.preventDefault();
+      window.location.href = "home.html"
+  });
+  document.getElementById("port").addEventListener("click", function(event){
+      event.preventDefault();
+      window.location.href = "portfolio..html"
+  });
+  document.getElementById("tentang").addEventListener("click", function(event){
+      event.preventDefault();
+      window.location.href = "home.html"
+  });
+  document.getElementById("con").addEventListener("click", function(event){
+    event.preventDefault();
+    window.location.href = "contact.html"
+});
